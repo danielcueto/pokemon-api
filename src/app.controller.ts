58561afba30fdb0768d67a0c09610c89e6ca9d69
danielcueto <daniel.cueto@assuresoft.tech>
2025-07-03 09:57:03 -0400

    fast

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('docker')
+  dockerInfo(): string {
+    return 'endpoint creado para ejecutar mis pipelines';
+  }
 }
